13667e25b0dba84bbad3be79fbde9261d60c86a1
Annazjulias <anazjuliasilva15@gmail.com>
2026-01-28 11:24:24 -0300

    localhost render

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,20 @@
+
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3002'
-})
-
+  baseURL: import.meta.env.VITE_API_URL
+});
 
 
 
 api.interceptors.request.use(async (config) => {
   const userDate = localStorage.getItem('devburguer:userData');
   const token = userDate && JSON.parse(userDate).token;
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
