750e2a70ef96647d82c634ef249f53767cdf29b7
Annazjulias <anazjuliasilva15@gmail.com>
2026-06-15 00:17:36 -0300

    ultima-atualizacao

--- src/components/CategoryCarousel/index.jsx
+++ src/components/CategoryCarousel/index.jsx
@@ -4,11 +4,20 @@ import { api } from '../../services/api'
 import { useEffect, useState } from "react";
 import { ContainerItens, Container, Title, CategoryButton } from "./style";
 import { useNavigate } from "react-router-dom";
+import img from "../../assets/category_1.png"
+import img2 from "../../assets/category_2.png"
+import img3 from "../../assets/category_3.png"
+import img4 from "../../assets/category_4.png"
 
 export function CategoryCarousel() {
 
   const [categories, setCategories] = useState([]);
-
+  const categoryImages = {
+    1: img,
+    2: img2,
+    3: img3,
+    4: img4,
+  };
   const navigate = useNavigate();
 
 
@@ -53,7 +62,7 @@ export function CategoryCarousel() {
         itemClass="carousel-item"
       >
         {categories.map((category) => (
-          <ContainerItens key={category.id} $imageUrl={category.url}>
+          <ContainerItens key={category.id} $imageUrl={categoryImages[category.id]}>
             <CategoryButton
               onClick={() => navigate(`/cardapio?category=${category.id}`)}
             >
